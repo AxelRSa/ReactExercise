e270028ef42478b5fdcdcf752d3aaa1dd1891cb8
AxelRSa <axelg.rizos@gmail.com>
2021-06-10 22:44:16 -0500

    Pasar objetos y variables a través del props

--- src/App.js
+++ src/App.js
@@ -3,12 +3,25 @@ import "./App.css";
 import Saludar from "./componentes/Saludar";
 
 function App() {
+ // const userName = "Axel Rizo";
+ // const edad = 26;
+
+ const user = {
+  nombre: "Betania Aguilar",
+  edad: 50,
+  color: "Azul",
+ };
+
  return (
   <div className="App">
    <header className="App-header">
     <img src={logo} className="App-logo" alt="logo" />
-    <Saludar name="Axel Rizo"  edad="500"/>
-    <Saludar name="Betania Aguilar" edad="25" />
+    {/* Meter variables de forma normal */}
+    {/* <Saludar name="Betania Aguilar" edad="25" /> */}
+    {/* Meter variables a través de variables */}
+    {/* <Saludar name={userName} edad={edad} /> */}
+    {/* Meter variables a través de objeto */}
+    <Saludar userinfo={user} />
    </header>
   </div>
  );
--- src/componentes/Saludar.js
+++ src/componentes/Saludar.js
@@ -1,11 +1,15 @@
 import React from "react";
 
 export default function Saludar(props) {
- console.log(props);
+ console.log(props.userinfo);
  return (
   <div>
    <h2>
-    Hola {props.name}, y tienes {props.edad} años
+    {/* Meter variables con el props normal */}
+    {/* Hola {props.name}, y tienes {props.edad} años */}
+    {/* Meter variables con el props a través de un objeto */}
+    Hola {props.userinfo.nombre}, y tienes {props.userinfo.edad} años, su color
+    favorito es el {props.userinfo.color}
    </h2>
   </div>
  );
